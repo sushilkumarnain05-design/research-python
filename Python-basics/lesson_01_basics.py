bb3951114631ec22e95dfdf13f2fc84db1daa7a2
Sushil Kumar <sushilkumarnain05@gmail.com>
2026-08-20 15:34:18 +0530

    Added seebeck coefficient calculation

--- Python-basics/lesson_01_basics.py
+++ Python-basics/lesson_01_basics.py
@@ -26,3 +26,11 @@ def calculate_resistance (voltage, current):
     return voltage / current
 R = calculate_resistance (10,0.05)
 print (R)
+
+def calculate_seebeck (delta_V, delta_T):
+    S = delta_V / delta_T
+    return S
+delta_V = 0.012
+delta_T = 20
+S = calculate_seebeck(delta_V, delta_T)
+print (S)
