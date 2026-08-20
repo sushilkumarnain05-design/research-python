0537cbf9d0d15320d301d5e2b0fce5603f5ee2a3
Sushil Kumar <sushilkumarnain05@gmail.com>
2026-08-20 13:22:47 +0530

    Added first python-basics script

---
+++ Python-basics/lesson_01_basics.py
@@ -0,0 +1,28 @@
+# import numpy as np
+# import matplotlib as mpl
+# import scipy
+# import pybaselines
+
+# print("All libraries imported successfully!")
+
+print("NEW RUN")
+
+temperature = 300
+
+if temperature > 300:
+    print("High")
+else:
+    print("Low")
+
+temperature=200
+if temperature > 300:
+    print("high")
+elif temperature >250:
+    print("medium")
+else:
+    print("low")
+
+def calculate_resistance (voltage, current):
+    return voltage / current
+R = calculate_resistance (10,0.05)
+print (R)
