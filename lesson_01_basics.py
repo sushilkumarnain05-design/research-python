abb6435acc58dc5d7ffca2f3cf8ff9742db44480
Sushil Kumar <sushilkumarnain05@gmail.com>
2026-08-22 19:33:21 +0530

    Add Python basics and NumPy fundamentals

---
+++ lesson_01_basics.py
@@ -0,0 +1,35 @@
+# import numpy as np
+# import matplotlib as mpl
+# import scipy
+# import pybaselines
+
+# print("All libraries imported successfully!")
+
+print("NEW RUN")
+
+temperature = 300
+
+if temperature > 300:
+    print("High")
+else:
+    print("Low")
+
+temperature=200
+if temperature > 300:
+    print("high")
+elif temperature >250:
+    print("medium")
+else:
+    print("low")
+
+def calculate_resistance (voltage, current):
+    return voltage / current
+R = calculate_resistance (10,0.05)
+print (R)
+import numpy as np
+resistance = np.array ([120, 125, 132, 140, 150])
+print("average:",np.mean (resistance))
+print("maximum:", np.max(resistance))
+print("minimum:", np.min(resistance))
+print ("standard deviation:", np.std(resistance))
+
